98e10f1275051a2fade77c6471594eb60e9af7c7
Drew Moody <drew.moody@me.com>
2018-10-25 13:51:36 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# nodeknexpostgres_template
+
+This is a very simple template to quickly deploy a server and db to Heroku
+
+Built with:
+- node
+- express
+- knex
+
+Databases:
+local: sqlite3
+production: postgresql
---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/mealprepdb.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true,
+    ssl: true,
+  }
+
+};
